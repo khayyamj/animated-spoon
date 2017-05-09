75315be0ea80bb6d054cb96992bf0cf0696950c6
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-09 10:06:10 -0600

    using linting tool (standard)

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,22 +1,31 @@
 /* global React ReactDOM */
 
-  var div = React.DOM.div
-  var h1 = React.DOM.h1
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
-  var MyTitle = React.createClass({
-    render: function() {
-      return (
-        div(null,
-          h1(null, "Check out my second component!")
-        )
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+         h1(null, this.props.title)
       )
-    }
-  })
-  var MyFirstComponent = (
-    div(null,
-      React.createElement(MyTitle, null),
-      React.createElement(MyTitle, null),
-      React.createElement(MyTitle, null)
     )
-  )
-  ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+// replace render method
+var MyFirstComponent = React.createClass({
+  render () {
+    return (
+      div(null,
+        MyTitleFact({title: 'Props are great!'}),
+        MyTitleFact({title: 'Use props everywhere!'}),
+        MyTitleFact({title: 'Props are the best!'})
+      )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
