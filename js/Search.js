19413230bb6877371e7a08e8368c21b467409ab4
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-09 13:39:20 -0600

    added search function

--- js/Search.js
+++ js/Search.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import preload from '../public/data.json'
+import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
   render () {
     return (
-      <h1>Search page!!</h1>
+      <div className='search'>
+        <header>
+          <h1>svideo</h1>
+          <input
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+            type='text'
+            placeholder='Search'
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(show => {
+              return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            })
+            .map(show => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+            )
+            })}
+        </div>
+      </div>
     )
   }
 })
